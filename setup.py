d72ead37a801badd822c8b24751c87d110831bf9
Vinayak Bhamblani <vinayakbhamblani4098@gmail.com>
2023-10-01 17:57:08 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+with open("README.md",'r',encoding="utf-8") as f:
+    long_description=f.read()
+__version__= "0.0.0"
+REPO_NAME="Text-Summarizer"   
+AUTHOR_USER_NAME="Vinayak2104"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL="vinayakbhamblani4098@gmail.com"
